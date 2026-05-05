4f13fd09b170d7d2374eac484095423315527b28
Anshuman0725 <anshuman0725@gmail.com>
2026-05-05 17:52:42 +0530

    commit

---
+++ day 2/Largest.cpp
@@ -0,0 +1,34 @@
+// largest from 3 numbers using nested if 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a >= b)
+    {
+        if (a >= c)
+        {
+            cout << a;
+        }
+        else
+        {
+            cout << c;
+        }
+    }
+    else
+    {
+        if (b >= c)
+        {
+            cout << b;
+        }
+        else
+        {
+            cout << c;
+        }
+    }
+
+    return 0;
+}
