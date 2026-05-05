bfa6f0279d1c1628d63e5c6d3ecf146130c58803
Anshuman0725 <anshuman0725@gmail.com>
2026-05-05 11:16:59 +0530

    commit

--- Day5_Functions/Coffee.cpp
+++ Day5_Functions/Coffee.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// Function to calculate total coffee volume
+int calculateVolume(int shots) {
+    return shots * 30;
+}
+
 int main() {
     int shots;
-    
+
     cout << "Enter number of coffee shots: ";
     cin >> shots;
 
-    int totalVolume = shots * 30;
+    int totalVolume = calculateVolume(shots);
 
     cout << "Total coffee volume = " << totalVolume << " ml" << endl;
 
